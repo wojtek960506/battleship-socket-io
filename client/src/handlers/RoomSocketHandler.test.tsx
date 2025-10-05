ef006cad418d36f66a8a3609fd08e08863f7af29
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-10-05 15:45:27 +0200

    more tests for RoomSocketHandler

--- client/src/handlers/RoomSocketHandler.test.tsx
+++ client/src/handlers/RoomSocketHandler.test.tsx
@@ -1,6 +1,6 @@
 import { act } from "@testing-library/react";
 import { renderWithMockSocket } from "../context/renderWithMockSocket"
-import { useRoomStore } from "../store/RoomStore";
+import { resetRoomStore, useRoomStore } from "../store/RoomStore";
 import { RoomSocketHandler } from "./RoomSocketHandler"
 
 type MockSocketType = {
@@ -9,27 +9,76 @@ type MockSocketType = {
   off: jest.Mock;
 }
 
-const socketEventHandler = (mockSocket: MockSocketType, eventName: string) => {
-  return mockSocket.on.mock.calls
-    .find(([name]) => name === eventName)![1]
+const triggerSocketEvent = (mockSocket: MockSocketType, eventName: string, args: any) => {
+  const handler = mockSocket.on.mock.calls
+    .find(([name]) => name === eventName)?.[1];
+  if (!handler) throw new Error(`Handler for '${eventName}' not found`);
+
+  act(() => handler(args))
 }
 
 describe("test RoomSocketHandler", () => {
 
-  const mockSocket: MockSocketType = { emit: jest.fn(), on: jest.fn(), off: jest.fn() }
+  let mockSocket: MockSocketType;
 
-  
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockSocket = { emit: jest.fn(), on: jest.fn(), off: jest.fn() };
+    resetRoomStore();
+  })
+
 
   test("handling 'room:set-player'", () => {
-
     renderWithMockSocket(<RoomSocketHandler />, mockSocket);
-
-    const handler = socketEventHandler(mockSocket, "room:set-player");
     const PLAYER_ID = 'player1'
 
-    act(() => handler(PLAYER_ID)); 
+    triggerSocketEvent(mockSocket, "room:set-player", PLAYER_ID);
 
     const roomState = useRoomStore.getState();
     expect(roomState.player).toBe(PLAYER_ID);
+
+  })
+
+  test("handling 'room:created'", () => {
+    renderWithMockSocket(<RoomSocketHandler />, mockSocket);
+    const args = {
+      room: "roomName",
+      playerId: "player1"
+    }
+
+    triggerSocketEvent(mockSocket, "room:created", args);
+
+    const roomState = useRoomStore.getState();
+    expect(roomState.roomName).toBe(args.room);
+    expect(roomState.players).toStrictEqual([{ id: args.playerId }]);
+    expect(roomState.status).toBe("waiting");
+    expect(roomState.errorMessage).toBe("");
+    expect(roomState.playerWhoLeft).toBe(null);
+  })
+
+  test("handling 'room:already-exists", () => {
+    renderWithMockSocket(<RoomSocketHandler />, mockSocket);
+    const args = { message: 'error' }
+
+    triggerSocketEvent(mockSocket, "room:already-exists", args);
+
+    const roomState = useRoomStore.getState();
+    expect(roomState.errorMessage).toBe(args.message);
+  })
+
+  test("handling 'room:you-joined", () => {
+    const PLAYER_ID = 'player2'
+    act(() => useRoomStore.getState().setPlayer(PLAYER_ID));
+    renderWithMockSocket(<RoomSocketHandler />, mockSocket);
+    const args = { room: 'room1', ownerId: '' }
+    
+    triggerSocketEvent(mockSocket, "room:you-joined", args);
+
+    const roomState = useRoomStore.getState();
+    expect(roomState.roomName).toBe(args.room);
+    expect(roomState.status).toBe("ready");
+    expect(roomState.errorMessage).toBe('');
+    expect(roomState.player).toBe(PLAYER_ID);
+    expect(roomState.players).toEqual([{ id: args.ownerId }, { id: PLAYER_ID }]);
   })
 })
