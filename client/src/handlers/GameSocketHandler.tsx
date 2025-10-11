20e7aa1354797252b9bd1db66bcf93967479ca68
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-10-11 16:56:10 +0200

    small logic update for 'player:board-set' handler

--- client/src/handlers/GameSocketHandler.tsx
+++ client/src/handlers/GameSocketHandler.tsx
@@ -1,17 +1,21 @@
 import { useEffect } from "react";
 import { useSocket } from "../context/SocketContext";
 import { useGameStore } from "../store/GameStore";
+import { useRoomStore } from "../store/RoomStore";
 
 
 export const GameSocketHandler = () => {
   const socket = useSocket();
   const { setIsOtherBoardSet, gameStatus, setGameStatus } = useGameStore();
+  const { player } = useRoomStore();
 
   useEffect(() => {
-    socket.on("player:board-set", ({ player }: { player: string}) => {
+    socket.on("player:board-set", ({ player: otherPlayer }: { player: string}) => {
       
       console.log("player:board-set - game status", gameStatus);
-      
+
+      if (player === otherPlayer) return;
+
       setIsOtherBoardSet(true);
       if (gameStatus === "board-set") {
         setGameStatus("playing")
