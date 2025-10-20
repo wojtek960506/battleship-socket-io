d8178f9a3cd4bf62a451137c207c6d75745eebf0
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-10-20 22:01:02 +0200

    add rest of tests to `useShotHandlers` * TODO now only unit tests left to write are for whole directory `src/components/Game`

--- client/src/hooks/useGameSocketHandlers/useShotHandlers.test.ts
+++ client/src/hooks/useGameSocketHandlers/useShotHandlers.test.ts
@@ -3,12 +3,10 @@ import { resetGameStore, useGameStore } from "@/store/GameStore";
 import { useRoomStore } from "@/store/RoomStore";
 import { COMMON_SHIP_1, COMMON_SHIP_2, getBoardWithCommonShips, OTHER_PLAYER_ID, PLAYER_ID } from "@/test-utils/constants";
 import { getHookResult, type HookResult } from "@/test-utils/getHookResult";
+import type { BoardCellType, ReceiveShotResultType } from "@/types";
 import { useShotHandlers } from "./useShotHandlers";
-import { getEmptyBoard } from "@/utils/board";
-import type { BoardCellType } from "@/types";
 
 describe("useShotHandlers", () => {
-
   let result: HookResult<typeof useShotHandlers>;
 
   beforeEach(() => {
@@ -20,10 +18,10 @@ describe("useShotHandlers", () => {
     jest.clearAllMocks();
   })
 
-  test("handleReceiveShot - the same player", () => {
-    act(() => result.current.handleReceiveShot(
-      { row: 1, column: 1, playerFromServer: PLAYER_ID }
-    ));
+  test.each<[handler: "handleReceiveShot" | "handleReceiveShotResult"]>(
+    [ ["handleReceiveShot"], ["handleReceiveShotResult"] ]
+  )("%s - the same player", (method) => {
+    act(() => result.current[method]({ playerFromServer: PLAYER_ID } as any));
 
     const gameState = useGameStore.getState();
     const initialGameState = useGameStore.getInitialState();
@@ -59,8 +57,60 @@ describe("useShotHandlers", () => {
 
     act(() => {
       expect(() => result.current.handleReceiveShot(
-      { row: 4, column: 5, playerFromServer: OTHER_PLAYER_ID }
-    )).toThrow(Error)
+        { row: 4, column: 5, playerFromServer: OTHER_PLAYER_ID }
+      )).toThrow(Error)
     })
+  })
+
+  test("handleReceiveShot - missed shot", () => {
+    act(() => useGameStore.getState().setOpponentBoard(getBoardWithCommonShips()));
+    
+    const data: ReceiveShotResultType = {
+      row: 1,
+      column: 1,
+      value: "missed",
+      playerFromServer: OTHER_PLAYER_ID,
+      sunkCells: [],
+      isFinished: false
+    }
+    act(() => result.current.handleReceiveShotResult(data));
+    
+    const board = getBoardWithCommonShips();
+    board[1][1] = "missed";
+
+    const gameState = useGameStore.getState();
+    expect(gameState.opponentBoard).toEqual(board);
+    expect(gameState.gameStatus).not.toBe("finished");
+    expect(gameState.winner).toBeNull();
+    expect(gameState.currentPlayer).toBe(OTHER_PLAYER_ID);
+  })
+
+  test("handleReceiveShot - sunk the last ship", () => {
+    act(() => useGameStore.getState().setOpponentBoard(getBoardWithCommonShips()));
+    
+    const data: ReceiveShotResultType = {
+      row: 6,
+      column: 5,
+      value: "sunk",
+      playerFromServer: OTHER_PLAYER_ID,
+      sunkCells: [
+        { row: 4, column: 5 },
+        { row: 5, column: 5 },
+        { row: 6, column: 5 },
+      ],
+      isFinished: true
+    }
+    act(() => result.current.handleReceiveShotResult(data));
+    
+    const board = getBoardWithCommonShips();
+    board[4][5] = "sunk";
+    board[5][5] = "sunk";
+    board[6][5] = "sunk";
+
+    const gameState = useGameStore.getState();
+    expect(gameState.opponentBoard).toEqual(board);
+    expect(gameState.gameStatus).toBe("finished");
+    expect(gameState.winner).toBe(PLAYER_ID);
+    expect(gameState.currentPlayer).toBeNull();
   })
 })
